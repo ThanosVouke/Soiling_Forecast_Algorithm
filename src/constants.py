c067e8530b32a896b0ca3678c7e13f26a2ea6b10
ThanosVouke <144725911+ThanosVouke@users.noreply.github.com>
2023-09-25 17:55:53 +0300

    Constant variables

---
+++ src/constants.py
@@ -0,0 +1,31 @@
+pi=3.14 # rho_aero: particles material density, kg/m^3
+
+rho_aero = 2650 # density from dessert dust   rho_aero=ρaero
+
+rho = 2500   # rho_air: density of the air, kg/m^3
+
+rho_air = 1.204 # h_air: dynamic viscosity of the air, kg/m s    rho_air=ρair
+
+h_air = 1.825e-05
+
+v_air = 1.516e-05   #kinematic viscosity of the air, m^2/s
+
+Particle_size_d_p = 6.25e-06   # einai 2.5 mexri 10 pm montelo 1
+
+Particle_size_d_p2p5 = 1.25e-06  # mexri 2.5 montelo 1 (meses times aytwn)
+
+Particle_size_d_p029 = 0.29e-06  # einai gia 0.03 me 0.55 montelo2 
+
+Particle_size_d_p07 = 0.7e-06  # einai gia 0.55 mexri 0.9 montelo2
+
+Particle_size_d_p10 = 10.45e-06 # einai gia 0.9 mexri 20  montelo2
+
+#%% Bearing angle 
+
+Location_bear_angle = 156.293 
+
+Location_lat= float(34.69)
+
+Location_lon= float(33.07)
+
+Location_Alt = 0.1
